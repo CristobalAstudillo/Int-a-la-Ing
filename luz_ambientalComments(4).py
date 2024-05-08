5ea16f639ba8147629b634028b357feb7a9bba38
Cristobal Astudillo <169069409+lilcrixx01@users.noreply.github.com>
2024-05-08 12:05:37 -0400

    luz_ambientalComments(4).py

--- luz_ambientalComments(4).py
+++ luz_ambientalComments(4).py
@@ -9,14 +9,12 @@ fotoR = machine.ADC(27)  #Pin en el cual está la fotoresistencia y del cual se 
 calcu= Pin(26, Pin.OUT)  #Pin en el cual está conectada la calculadora y lo deja como un pin de salida
 
 while True:
-
                                        # Lee el valor de la resistencia y lo convierte a un valor de 16 bits.
     luz_ambiental = fotoR.read_u16()   # Entre más luz incida en la superficie de la fotoresistencia
                                        # menor será el valor leído. Y si hay poca/nula luz incidiendo en la superficie, el valor leído será mayor.
-                                       
+   
     print("Valor de la fotoresistencia:", luz_ambiental)  # Imprime el valor que da la resistenia
     
-
     if luz_ambiental < mucha_luz:
         print("Hay mucha luz")
     if luz_ambiental > mucha_luz and luz_ambiental < luz_moderada:
@@ -26,6 +24,7 @@ while True:
     if luz_ambiental > poca_luz:
         print("No hay luz")  #Si el valor de la fotoresistencia es mayor al valor 23k, no hay luz.
 
+   
     if luz_ambiental > poca_luz:
         print("Apagando calculadora")
         calcu.value(0) #Si luz ambiental es mayor o igual al valor de 23k, apague el pin 26
